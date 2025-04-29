4821b26a74219a33ecd6ebed0319d90021bc837c
3RESHE <jannerish@gmail.com>
2025-04-30 01:21:36 +0800

    reordered the component examples

--- web/pages/component/index.tsx
+++ web/pages/component/index.tsx
@@ -42,6 +42,19 @@ export default function Home() {
           <div className="flex flex-wrap mt-4">
             <div 
               className="block basis-1/2 md:basis-1/3 text-center cursor-pointer"
+              onClick={() => router.push('/component/accordion')} 
+            >
+              <div className="m-2 border border-b2 rounded overflow-hidden">
+                <div className="flex items-center justify-center h-[100px] w-full bg-b1 px-3">
+                  Unlocks at 10,000 downloads
+                </div>
+                <h2 className="my-2 font-semibold text-center uppercase">
+                  {_('Accordion')}
+                </h2>
+              </div>
+            </div>
+            <div 
+              className="block basis-1/2 md:basis-1/3 text-center cursor-pointer"
               onClick={() => router.push('/component/alert')} 
             >
               <div className="m-2 border border-b2 rounded overflow-hidden">
@@ -66,6 +79,19 @@ export default function Home() {
                 </div>
                 <h2 className="my-2 font-semibold text-center uppercase">
                   {_('Badge')}
+                </h2>
+              </div>
+            </div>
+            <div 
+              className="block basis-1/2 md:basis-1/3 text-center cursor-pointer"
+              onClick={() => router.push('/component/bread-crumb')} 
+            >
+              <div className="m-2 border border-b2 rounded overflow-hidden">
+                <div className="flex items-center justify-center h-[100px] w-full bg-b1 px-3">
+                  Unlocks at 2,000 downloads
+                </div>
+                <h2 className="my-2 font-semibold text-center uppercase">
+                  {_('Bread Crumb')}
                 </h2>
               </div>
             </div>
@@ -104,13 +130,39 @@ export default function Home() {
                   <div className="rounded overflow-hidden w-full">
                     <header className="flex items-center bg-b1 p-2">
                       <h3 className="flex-grow uppercase font-semibold">Confirm</h3>
-                      <a href="#" className="float-right font-bold">&times;</a>
+                      <a href="#" className="float-right font-bold">×</a>
                     </header>
                     <div className="bg-b2 p-2">Are You Sure?</div>
                   </div>
                 </div>
                 <h2 className="my-2 font-semibold text-center uppercase">
                   {_('Modal')}
+                </h2>
+              </div>
+            </div>
+            <div 
+              className="block basis-1/2 md:basis-1/3 text-center cursor-pointer"
+              onClick={() => router.push('/component/pagination')} 
+            >
+              <div className="m-2 border border-b2 rounded overflow-hidden">
+                <div className="flex items-center justify-center h-[100px] w-full bg-b1 px-3">
+                  Unlocks at 4,000 downloads
+                </div>
+                <h2 className="my-2 font-semibold text-center uppercase">
+                  {_('Pagination')}
+                </h2>
+              </div>
+            </div>
+            <div 
+              className="block basis-1/2 md:basis-1/3 text-center cursor-pointer"
+              onClick={() => router.push('/component/progress-bar')} 
+            >
+              <div className="m-2 border border-b2 rounded overflow-hidden">
+                <div className="flex items-center justify-center h-[100px] w-full bg-b1 px-3">
+                  Unlocks at 6,000 downloads
+                </div>
+                <h2 className="my-2 font-semibold text-center uppercase">
+                  {_('Progress Bar')}
                 </h2>
               </div>
             </div>
@@ -136,47 +188,10 @@ export default function Home() {
                 </h2>
               </div>
             </div>
-          </div>
-          <h2 className="px-3 flex items-center uppercase font-bold text-xl mt-4">
-            <i className="fas fa-lock mr-2" />
-            {_('Locked')}
-          </h2>
-          <p className="px-3 pt-3">
-            The following components have are locked until enough there are 
-            enough users and demand for such components.
-          </p>
-          <div className="flex flex-wrap mt-4">
-            <div className="block basis-1/2 md:basis-1/3 text-center cursor-pointer">
-              <div className="m-2 border border-b2 rounded overflow-hidden">
-                <div className="flex items-center justify-center h-[100px] w-full bg-b1 px-3">
-                  Unlocks at 2,000 downloads
-                </div>
-                <h2 className="my-2 font-semibold text-center uppercase">
-                  {_('Bread Crumb')}
-                </h2>
-              </div>
-            </div>
-            <div className="block basis-1/2 md:basis-1/3 text-center cursor-pointer">
-              <div className="m-2 border border-b2 rounded overflow-hidden">
-                <div className="flex items-center justify-center h-[100px] w-full bg-b1 px-3">
-                  Unlocks at 4,000 downloads
-                </div>
-                <h2 className="my-2 font-semibold text-center uppercase">
-                  {_('Pagination')}
-                </h2>
-              </div>
-            </div>
-            <div className="block basis-1/2 md:basis-1/3 text-center cursor-pointer">
-              <div className="m-2 border border-b2 rounded overflow-hidden">
-                <div className="flex items-center justify-center h-[100px] w-full bg-b1 px-3">
-                  Unlocks at 6,000 downloads
-                </div>
-                <h2 className="my-2 font-semibold text-center uppercase">
-                  {_('Progress Bar')}
-                </h2>
-              </div>
-            </div>
-            <div className="block basis-1/2 md:basis-1/3 text-center cursor-pointer">
+            <div 
+              className="block basis-1/2 md:basis-1/3 text-center cursor-pointer"
+              onClick={() => router.push('/component/tabs')} 
+            >
               <div className="m-2 border border-b2 rounded overflow-hidden">
                 <div className="flex items-center justify-center h-[100px] w-full bg-b1 px-3">
                   Unlocks at 8,000 downloads
@@ -186,33 +201,29 @@ export default function Home() {
                 </h2>
               </div>
             </div>
-            <div className="block basis-1/2 md:basis-1/3 text-center cursor-pointer">
-              <div className="m-2 border border-b2 rounded overflow-hidden">
-                <div className="flex items-center justify-center h-[100px] w-full bg-b1 px-3">
-                  Unlocks at 10,000 downloads
-                </div>
-                <h2 className="my-2 font-semibold text-center uppercase">
-                  {_('Accordion')}
-                </h2>
-              </div>
-            </div>
-            <div className="block basis-1/2 md:basis-1/3 text-center cursor-pointer">
-              <div className="m-2 border border-b2 rounded overflow-hidden">
-                <div className="flex items-center justify-center h-[100px] w-full bg-b1 px-3">
-                  Unlocks at 12,000 downloads
-                </div>
-                <h2 className="my-2 font-semibold text-center uppercase">
-                  {_('Tooltip')}
-                </h2>
-              </div>
-            </div>
-            <div className="block basis-1/2 md:basis-1/3 text-center cursor-pointer">
+            <div 
+              className="block basis-1/2 md:basis-1/3 text-center cursor-pointer"
+              onClick={() => router.push('/component/toast')} 
+            >
               <div className="m-2 border border-b2 rounded overflow-hidden">
                 <div className="flex items-center justify-center h-[100px] w-full bg-b1 px-3">
                   Unlocks at 14,000 downloads
                 </div>
                 <h2 className="my-2 font-semibold text-center uppercase">
                   {_('Toast')}
+                </h2>
+              </div>
+            </div>
+            <div 
+              className="block basis-1/2 md:basis-1/3 text-center cursor-pointer"
+              onClick={() => router.push('/component/tooltip')} 
+            >
+              <div className="m-2 border border-b2 rounded overflow-hidden">
+                <div className="flex items-center justify-center h-[100px] w-full bg-b1 px-3">
+                  Unlocks at 12,000 downloads
+                </div>
+                <h2 className="my-2 font-semibold text-center uppercase">
+                  {_('Tooltip')}
                 </h2>
               </div>
             </div>

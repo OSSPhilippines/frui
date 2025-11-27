9755f39e4be29218e5ae94a5c1d5a61fe478ebfa
Joseph Louise <josephdeleonpogi@gmail.com>
2025-11-27 13:28:02 +0800

    Refactoring component imports and updating format names for consistency.

--- components/form/index.ts
+++ components/form/index.ts
@@ -28,13 +28,13 @@ import Fieldset from './Fieldset.js';
 export type { FileInputProps } from './FileInput.js';
 import FileInput from './FileInput.js';
 
-export type {  } from './ImageInput.js';
+export type { } from './ImageInput.js';
 import ImageInput from './ImageInput.js';
 
 export type { FilelistProps } from './FileList.js';
 import FileList from './FileList.js';
 
-export type {  } from './ImageList.js';
+export type { } from './ImageList.js';
 import ImageList from './ImageList.js';
 
 export type { InputProps } from './Input.js';
